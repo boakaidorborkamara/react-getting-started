931120cbc041c622baa027b9553c085338100b23
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-08-14 23:01:35 +0000

    created a Table component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-class App extends React.Component{
-  render(){
-    return(
-      <h1>Hello World</h1>
-    )
-  }
-}
+import './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
